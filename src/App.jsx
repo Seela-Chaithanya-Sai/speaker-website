58c82d9be251821dcfe93d4cf7eba332eeec8208
Seela Chaithanya Sai <seelachaithanyasai@gmail.com>
2026-08-14 18:19:00 +0530

    Fix Navbar import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from
 "react-router-dom";
 
-import Navbar  from "./components/Navbar";
+import Navbar  from "./components/Navbar.jsx";
 import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
